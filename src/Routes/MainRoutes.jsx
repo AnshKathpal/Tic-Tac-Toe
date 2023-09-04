d4097cba1d27e74722d313a0ad7ba4cf56e40f61
AnshKathpal <storage.2.3107@gmail.com>
2023-09-04 23:54:52 +0530

    game draw

---
+++ src/Routes/MainRoutes.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+import React from "react";
+import { Home } from "../Pages/Home";
+import { Board } from "../Structure/Board";
+
+export const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/game" element={<Board />} />
+    </Routes>
+  );
+};
